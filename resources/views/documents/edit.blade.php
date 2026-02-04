3fe0124cf6cbfd56eaad7f671949985d28145bb6
aitaddihamza <aitaddi.hamza365@gmail.com>
2026-02-04 14:41:42 +0100

    hope this fixes the problme

--- resources/views/documents/edit.blade.php
+++ resources/views/documents/edit.blade.php
@@ -14,8 +14,7 @@
         } = globalThis;
 
         // The workerSrc property shall be specified.
-        pdfjsLib.GlobalWorkerOptions.workerSrc = "{{ asset('js/pdf.worker.mjs') }}";
-        // pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdn.jsdelivr.net/npm/pdfjs-dist@5.4.530/build/pdf.worker.mjs';
+        pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdn.jsdelivr.net/npm/pdfjs-dist@5.4.530/build/pdf.worker.mjs';
 
         const Y_FROM_TOP = true;
         // ============================================
