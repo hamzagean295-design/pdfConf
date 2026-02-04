1e2894981e4aae7668a2df7cbe0d2236d5c38a30
aitaddihamza <aitaddi.hamza365@gmail.com>
2026-02-04 16:06:14 +0100

    playing with tailwindc a little bit

--- resources/views/documents/edit.blade.php
+++ resources/views/documents/edit.blade.php
@@ -343,8 +343,8 @@
         })" x-cloak class="flex h-screen bg-red-100">
 
             <!-- Colonne de gauche : Liste des éléments -->
-            <aside class="h-full flex-1 flex flex-col border-r bg-white">
-                <div class="p-4 border-b">
+            <aside class="h-full flex-1 flex flex-col bg-white">
+                <div class="p-4">
                     <button @click="addElement()"
                         class="w-full bg-blue-500 text-white font-bold py-2 px-4 rounded cursor-pointer">
                         Ajouter un Élément
@@ -353,7 +353,7 @@
                 <div class="flex-grow overflow-y-auto">
                     <template x-for="element in elements" :key="element.id">
                         <div @click="selectElement(element.id)"
-                            class="p-3 border-b cursor-pointer hover:bg-gray-50 flex justify-between items-center"
+                            class="shadow-sm p-3  mt-1 cursor-pointer hover:bg-gray-50 flex justify-between items-center"
                             :class="{ 'bg-blue-100 hover:bg-blue-100': selectedId === element.id }">
                             <div>
                                 <!-- Affiche le label ou la valeur si pas de label -->
@@ -372,7 +372,7 @@
 
             <!-- Colonne du centre : Prévisualisation PDF -->
             <main class="flex-3 relative h-full flex flex-col bg-gray-200">
-                <canvas id="the-canvas" class="border-2 border-gray-400 shadow-lg"></canvas>
+                <canvas id="the-canvas" class="border-2 border-blue-400 shadow-lg"></canvas>
                 <div class="flex items-center justify-center gap-3 mt-4">
                     <button type="button"
                         class="border px-4 py-2 cursor-pointer rounded bg-gray-100 hover:bg-gray-200 font-medium"
@@ -387,7 +387,7 @@
                 </div>
             </main>
             <!-- Colonne de droite : Éditeur de propriétés -->
-            <aside class="h-full flex-1 flex flex-col border-l bg-white">
+            <aside class="h-full flex-1 flex flex-col  bg-white">
                 <div class="p-4 overflow-y-auto space-y-4">
                     <h2 class="text-sm font-semibold uppercase tracking-wide text-gray-700">
                         Propriétés
@@ -400,27 +400,17 @@
                     <div x-show="selectedId" x-if="selectedElement()" class="space-y-5">
 
                         <!-- Label + Type -->
-                        <div class="grid grid-cols-3 gap-3">
+                        <div class="grid grid-cols-2 gap-3">
                             <div>
                                 <label class="block text-xs text-gray-500 mb-1">Label</label>
                                 <input type="text" x-model="selectedElement().label"
-                                    class="w-full h-9 px-2 text-sm border rounded">
+                                    class="block w-full rounded-md border-gray-300 p-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                             </div>
 
                             <div>
-                                <label class="block text-xs text-gray-500 mb-1">Type</label>
-                                <select x-model="selectedElement().type"
-                                    @change="if(selectedElement().type === 'checkbox' && !selectedElement().options) selectedElement().options = []"
-                                    class="w-full h-9 px-2 text-sm border rounded">
-                                    <option value="text">Texte</option>
-                                    <option value="tag">Tag</option>
-                                    <option value="checkbox">Checkbox</option>
-                                </select>
-                            </div>
-                            <div>
                                 <label class="block text-xs text-gray-500 mb-1">Page</label>
                                 <select x-model.number="selectedElement().page"
-                                    class="w-full h-9 px-2 text-sm border rounded">
+                                    class="block w-full rounded-md border-gray-300 p-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                                     <template x-for="i in totalPages" :key="i">
                                         <option :value="i" x-text="i"></option>
                                     </template>
@@ -429,18 +419,28 @@
                         </div>
 
                         <!-- Page + Valeur -->
-                        <div class="grid grid-cols-2 gap-3">
+                        <div class="grid grid-cols-2 gap-3 justify-between">
+                            <div>
+                                <label class="block text-xs text-gray-500 mb-1">Type</label>
+                                <select x-model="selectedElement().type"
+                                    @change="if(selectedElement().type === 'checkbox' && !selectedElement().options) selectedElement().options = []"
+                                    class="block w-full rounded-md border-gray-300 p-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
+                                    <option value="text">Texte</option>
+                                    <option value="tag">Tag</option>
+                                    <option value="checkbox">Checkbox</option>
+                                </select>
+                            </div>
                             <div>
                                 <label class="block text-xs text-gray-500 mb-1">Valeur</label>
                                 <input type="text" x-model.debounce.200ms="selectedElement().value"
                                     :placeholder="selectedElement().type === 'checkbox' ? '@{{ user.gender }}' :
                                         'Texte ou @{{ tag }}'"
-                                    class="w-full h-9 px-2 text-sm border rounded">
+                                    class="block w-full rounded-md border-gray-300 p-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                             </div>
                             <div x-show="selectedElement().type == 'tag' || selectedElement().type == 'checkbox' ">
                                 <label class="block text-xs text-gray-500 mb-1">Valeur de test</label>
                                 <input type="text" x-model.debounce.200ms="selectedElement().valueTest"
-                                    class="w-full h-9 px-2 text-sm border rounded">
+                                    class="block w-full rounded-md border-gray-300 p-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                             </div>
                         </div>
 
@@ -449,18 +449,18 @@
                             <div>
                                 <label class="block text-xs text-gray-500 mb-1">X (mm)</label>
                                 <input type="number" step="0.1" id="cord-x" x-model.number="selectedElement().x"
-                                    class="w-full h-9 px-2 text-sm border rounded">
+                                    class="block w-full rounded-md border-gray-300 p-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                             </div>
 
                             <div>
                                 <label class="block text-xs text-gray-500 mb-1">Y (mm)</label>
                                 <input type="number" id="cord-y" step="0.1" x-model.number="selectedElement().y"
-                                    class="w-full h-9 px-2 text-sm border rounded">
+                                    class="block w-full rounded-md border-gray-300 p-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                             </div>
 
                             <button id="demarrer" type="button"
-                                class="h-9 text-sm border rounded bg-gray-100 hover:bg-gray-200">
-                                Définir
+                                class="h-9 flex items-center justify-center text-sm border cursor-pointer z-20 rounded-md sm:text-sm">
+                                <img src="{{ asset('target.png') }}" alt="target" width="30" class="z-10">
                             </button>
                         </div>
 
@@ -474,7 +474,7 @@
                                 <div>
                                     <label class="block text-xs text-gray-500 mb-1">Police</label>
                                     <select x-model.number="selectedElement().font_family"
-                                        class="w-full h-9 px-2 text-sm border rounded">
+                                        class="block w-full rounded-md border-gray-300 p-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                                         <template x-for="f in fonts" :key="f">
                                             <option :value="f" x-text="f"></option>
                                         </template>
@@ -484,13 +484,13 @@
                                 <div>
                                     <label class="block text-xs text-gray-500 mb-1">Taille</label>
                                     <input type="number" x-model.number="selectedElement().font_size"
-                                        class="w-full h-9 px-2 text-sm border rounded">
+                                        class="block w-full rounded-md border-gray-300 p-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                                 </div>
 
                                 <div>
                                     <label class="block text-xs text-gray-500 mb-1">Poids</label>
                                     <select x-model="selectedElement().font_weight"
-                                        class="w-full h-9 px-2 text-sm border rounded">
+                                        class="block w-full rounded-md border-gray-300 p-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                                         <option value="normal">Normal</option>
                                         <option value="bold">Gras</option>
                                     </select>
@@ -499,7 +499,7 @@
                                 <div>
                                     <label class="block text-xs text-gray-500 mb-1">Style</label>
                                     <select x-model="selectedElement().font_style"
-                                        class="w-full h-9 px-2 text-sm border rounded">
+                                        class="block w-full rounded-md border-gray-300 p-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                                         <option value="normal">Normal</option>
                                         <option value="italic">Italique</option>
                                     </select>
@@ -508,7 +508,7 @@
                                 <div class="col-span-2">
                                     <label class="block text-xs text-gray-500 mb-1">Couleur</label>
                                     <input type="color" :value="selectedElement().hexColor" @input="updateColor($event)"
-                                        class="w-full h-9 border rounded">
+                                        class="">
                                 </div>
                             </div>
                         </div>
@@ -520,12 +520,12 @@
                             </h3>
 
                             <button @click="addCheckboxOption()" type="button"
-                                class="w-full h-9 text-sm border rounded bg-blue-50 hover:bg-blue-100">
+                                class="w-full h-9 text-sm border rounded hover:bg-blue-100">
                                 + Ajouter une option
                             </button>
 
                             <template x-for="(option, index) in selectedElement().options" :key="index">
-                                <div class="p-3 border rounded bg-gray-50 space-y-2">
+                                <div class="p-3 shadow-md border rounded space-y-2">
                                     <div class="flex justify-between items-center text-xs font-medium">
                                         <span x-text="`Option ${index + 1}`"></span>
                                         <button @click="removeCheckboxOption(index)"
@@ -534,13 +534,13 @@
 
                                     <div class="grid grid-cols-2 gap-2">
                                         <input x-model="option.label" placeholder="Label"
-                                            class="h-8 px-2 text-sm border rounded">
+                                            class="block w-full rounded-md border-gray-300 p-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                                         <input x-model="option.value" placeholder="Valeur"
-                                            class="h-8 px-2 text-sm border rounded">
+                                            class="block w-full rounded-md border-gray-300 p-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                                         <input type="number" step="0.1" x-model.number="option.x" placeholder="X"
-                                            class="h-8 px-2 text-sm border rounded">
+                                            class="block w-full rounded-md border-gray-300 p-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                                         <input type="number" step="0.1" x-model.number="option.y" placeholder="Y"
-                                            class="h-8 px-2 text-sm border rounded">
+                                            class="block w-full rounded-md border-gray-300 p-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                                     </div>
                                 </div>
                             </template>
@@ -552,16 +552,16 @@
                 <div class="p-4 border-t">
                     <div class=" flex items-center gap-1">
                         <button @click="save()" :disabled="isSaving"
-                            class="w-full h-10 text-sm font-medium text-white bg-indigo-600 rounded disabled:bg-gray-400">
+                            class="inline-flex justify-center rounded-md border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
                             <span x-show="!isSaving">Sauvegarder</span>
                             <span x-show="isSaving">Sauvegarde...</span>
                         </button>
                         <a target="_blank" href="{{ route('documents.download', $document->id) }}"
-                            class="flex justify-center items-center  w-full h-10 text-sm font-medium text-white bg-emerald-600 rounded disabled:bg-gray-400">
+                            class="inline-flex justify-center rounded-md border border-transparent bg-emerald-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-offset-2">
                             Tester
                         </a>
                     </div>
-                    <i>sauvegarder avant de testet!</i>
+                    <i class="mt-2 text-red-700 block font-normal text-center mx-auto">sauvegarder avant le test!</i>
                 </div>
             </aside>
         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,13 +13,14 @@
 <body class="antialiased bg-gray-100 h-screen">
 
     <header class="bg-black">
-        <nav class="max-w-5xl mx-auto py-4">
+        <nav class="max-w-5xl pl-8 py-4">
             <ul class=" flex items-center gap-2">
                 <li><a class="text-white black:text-black text-xl" href="{{ route('documents.index') }}">Pdf Config
                         manager</a></li>
-                <li><a class="text-white black:text-black underline pl-8"
+                <li><a class="text-white font-bold black:text-black pl-8"
                         href="{{ route('factures.index') }}">Factures</a></li>
-                <li><a class="text-white black:text-black underline pl-8" href="{{ route('cnss.index') }}">Cnss</a></li>
+                <li><a class="text-white black:text-black flex justify-center font-bold pl-8"
+                        href="{{ route('cnss.index') }}">Cnss</a></li>
             </ul>
         </nav>
     </header>
